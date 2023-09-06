81c34eece58ed6bb6c20e5f9d6bcfad93f54fa22
Restyled.io <commits@restyled.io>
2023-09-06 12:20:04 +0000

    Restyled by clang-format

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const hamburgerButton = document.getElementById("hamburger");
 const navList = document.getElementById("nav-list");
 
-function toggleButton() {
-  navList.classList.toggle("show");
-}
+function toggleButton() { navList.classList.toggle("show"); }
 
 hamburgerButton.addEventListener("click", toggleButton);
