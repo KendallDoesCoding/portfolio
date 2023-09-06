9cf49806d112267607e9621b8afaf2ef3fd35a3b
Restyled.io <commits@restyled.io>
2023-09-06 12:20:08 +0000

    Restyled by prettier

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const hamburgerButton = document.getElementById("hamburger");
 const navList = document.getElementById("nav-list");
 
-function toggleButton() { navList.classList.toggle("show"); }
+function toggleButton() {
+  navList.classList.toggle("show");
+}
 
 hamburgerButton.addEventListener("click", toggleButton);
